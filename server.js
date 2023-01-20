42b5bc8ba3b39ceb601d3179202c6ce959b15846
williampena28 <56842242+williampena28@users.noreply.github.com>
2023-01-20 16:01:40 -0500

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
+let Item = require('./models/items.js');
 
+app.use(express.static('public'));
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+const connectionString = `mongodb+srv://${process.env.MONGOUSERNAME}:${process.env.MONGOPASSWORD}@mongosetupcluster.i58imot.mongodb.net/Inventory?retryWrites=true&w=majority`;
+mongoose.set('strictQuery', false);
+mongoose.connect(connectionString,
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+mongoose.connection.once('open', () =>
+{
+    console.log('Connected to mongo');
+})
 app.listen(5000, () =>
 {
     console.log('Server is listening on port 5000...');
-})
+});
